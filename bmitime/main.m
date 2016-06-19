f70da070b4519d0c94d9b0a73c001079ed329a83
Dan Patey <danpatey@danpatey.com>
2016-06-19 16:17:42 -0700

    Initial Commit

---
+++ bmitime/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  bmitime
+//
+//  Created by Dan Patey on 6/19/16.
+//  Copyright © 2016 Dan Patey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
